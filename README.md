2a877a9eb410ebc707403f1a11ccfffc93e04d2b
carlos jhardel <orellano428@gmail.com>
2024-02-05 15:58:10 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# API MATRICULA
+
+1. Clonar el proyecto
+2. `npm install`
+3. Clonar el archivo `.env.example` y renombrar a `.env`
+4. Cambiar la varibles de entorno
+5. Levantar la base de datos
+
+```
+docker-compose up -d
+
+```
+
+6. Levantar en desarrollo: `npm run start:dev`
---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
