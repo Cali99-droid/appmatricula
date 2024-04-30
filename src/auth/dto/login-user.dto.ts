38a747b7416f0745f64354c76c8405059eac0ee1
carlos jhardel <orellano428@gmail.com>
2024-04-29 23:00:59 -0500

    docs login

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsString,
@@ -7,10 +8,18 @@ import {
 } from 'class-validator';
 
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'email',
+    nullable: false,
+  })
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'password',
+    nullable: false,
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(50)
