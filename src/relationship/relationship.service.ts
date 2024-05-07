fa13a59ebb66ed2131c844a70c4badf909fbb59b
carlos jhardel <orellano428@gmail.com>
2024-05-07 00:52:41 -0500

    duplicate entry validate

--- src/relationship/relationship.service.ts
+++ src/relationship/relationship.service.ts
@@ -6,6 +6,7 @@ import { Person } from 'src/person/entities/person.entity';
 import { Relationship } from './entities/relationship.entity';
 import { DataParentArrayDto } from './dto/data-parent-array.dto';
 import { Student } from 'src/student/entities/student.entity';
+import { handleDBExceptions } from 'src/common/helpers/handleDBException';
 
 @Injectable()
 export class RelationshipService {
@@ -22,66 +23,70 @@ export class RelationshipService {
     const { data } = dataParentArrayDto;
 
     // Pre-fetch existing records to minimize DB calls
-    const docNumbers = data.map((item) => item.docNumber);
-    const existingPersons = await this.personRepository.find({
-      where: { docNumber: In(docNumbers) },
-    });
-    console.log('exist', existingPersons);
-    const existingDocsSet = new Set(existingPersons.map((p) => p.docNumber));
+    try {
+      const docNumbers = data.map((item) => item.docNumber);
+      const existingPersons = await this.personRepository.find({
+        where: { docNumber: In(docNumbers) },
+      });
 
-    const personsToSave = data.filter(
-      (item) => !existingDocsSet.has(item.docNumber),
-    );
-    let savedPersons = [];
-    console.log('to save', personsToSave);
-    const personsToSaveFormat = personsToSave.map((item) => {
-      return {
-        name: item.name,
-        lastname: item.lastname,
-        mLastname: item.mLastname,
-        docNumber: item.docNumber,
-        gender: item.gender,
-        familyRole: item.familyRole,
-      };
-    });
-    if (personsToSave.length > 0) {
-      savedPersons = await this.personRepository.save(
-        this.personRepository.create(personsToSaveFormat),
+      const existingDocsSet = new Set(existingPersons.map((p) => p.docNumber));
+
+      const personsToSave = data.filter(
+        (item) => !existingDocsSet.has(item.docNumber),
       );
-    }
+      let savedPersons = [];
 
-    const studentCodes = data.map((item) => ({
-      dniAssignee: item.docNumber,
-      sonStudentCode: item.studentCode,
-    }));
-    const existingFamilies = await this.relationshipRepository.find({
-      where: studentCodes,
-    });
-    const existingFamiliesSet = new Set(
-      existingFamilies.map((f) => `${f.dniAssignee}_${f.sonStudentCode}`),
-    );
+      const personsToSaveFormat = personsToSave.map((item) => {
+        return {
+          name: item.name,
+          lastname: item.lastname,
+          mLastname: item.mLastname,
+          docNumber: item.docNumber,
+          gender: item.gender,
+          familyRole: item.familyRole,
+        };
+      });
+      if (personsToSave.length > 0) {
+        savedPersons = await this.personRepository.save(
+          this.personRepository.create(personsToSaveFormat),
+        );
+      }
 
-    const familiesToSave = data.filter(
-      (item) =>
-        !existingFamiliesSet.has(`${item.docNumber}_${item.studentCode}`),
-    );
-    let savedFamilies = [];
-    const familiesToSaveFormat = familiesToSave.map((item) => {
-      return {
+      const studentCodes = data.map((item) => ({
         dniAssignee: item.docNumber,
         sonStudentCode: item.studentCode,
-      };
-    });
-    if (familiesToSave.length > 0) {
-      savedFamilies = await this.relationshipRepository.save(
-        this.relationshipRepository.create(familiesToSaveFormat),
+      }));
+      const existingFamilies = await this.relationshipRepository.find({
+        where: studentCodes,
+      });
+      const existingFamiliesSet = new Set(
+        existingFamilies.map((f) => `${f.dniAssignee}_${f.sonStudentCode}`),
       );
-    }
 
-    return {
-      savedFamiliesMembers: savedFamilies.length,
-      savedPersons: savedPersons.length,
-    };
+      const familiesToSave = data.filter(
+        (item) =>
+          !existingFamiliesSet.has(`${item.docNumber}_${item.studentCode}`),
+      );
+      let savedFamilies = [];
+      const familiesToSaveFormat = familiesToSave.map((item) => {
+        return {
+          dniAssignee: item.docNumber,
+          sonStudentCode: item.studentCode,
+        };
+      });
+      if (familiesToSave.length > 0) {
+        savedFamilies = await this.relationshipRepository.save(
+          this.relationshipRepository.create(familiesToSaveFormat),
+        );
+      }
+
+      return {
+        savedFamiliesMembers: savedFamilies.length,
+        savedPersons: savedPersons.length,
+      };
+    } catch (error) {
+      handleDBExceptions(error, this.logger);
+    }
   }
 
   async findAll() {
