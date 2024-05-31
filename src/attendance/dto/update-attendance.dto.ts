2f53aa0422ea1f841a81bc5134ddac9964279402
Adnner-Esperilla-Dev <adnnerer@hotmail.com>
2024-05-31 15:21:43 -0500

    dto update

--- src/attendance/dto/update-attendance.dto.ts
+++ src/attendance/dto/update-attendance.dto.ts
@@ -1,4 +1,27 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAttendanceDto } from './create-attendance.dto';
+// import { PartialType } from '@nestjs/swagger';
+// import { CreateAttendanceDto } from './create-attendance.dto';
 
-export class UpdateAttendanceDto extends PartialType(CreateAttendanceDto) {}
+// export class UpdateAttendanceDto extends PartialType(CreateAttendanceDto) {}
+import { ApiProperty } from '@nestjs/swagger';
+import { ConditionAttendance } from '../enum/condition.enum';
+import { IsEnum } from 'class-validator';
+
+export class UpdateAttendanceDto {
+  // @ApiProperty({
+  //   example: 'P',
+  //   description: 'type of attendance, must be P, T or F',
+  //   type: 'enum',
+  //   enum: ConditionAttendance,
+  // })
+  // condition: ConditionAttendance;
+  @ApiProperty({
+    description: 'Condition of attedance',
+    nullable: false,
+    example: '1',
+    enum: ConditionAttendance,
+  })
+  @IsEnum(ConditionAttendance, {
+    message: 'type of attendance, must be P, T or F',
+  })
+  condition: ConditionAttendance;
+}
