9a4ac7e6277be2dac5afc8296597b3709182a960
carlos jhardel <orellano428@gmail.com>
2024-06-13 00:35:46 -0500

    change tokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -98,7 +98,7 @@ export class AuthService {
     const [accessToken, refreshToken] = await Promise.all([
       this.jwtService.signAsync(payload, {
         secret: this.configService.get<string>('JWT_SECRET'),
-        expiresIn: '15m',
+        expiresIn: '5h',
       }),
       this.jwtService.signAsync(payload, {
         secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
