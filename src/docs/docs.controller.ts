800c9abfa5379345c36e978b294a291398e43817
Adnner <adnnerer@hotmail.com>
2024-09-24 21:07:43 -0500

    [CREATE ] Pdf Contract

--- src/docs/docs.controller.ts
+++ src/docs/docs.controller.ts
@@ -3,6 +3,9 @@ import { Controller, Get, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { PdfService } from './pdf.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { User } from 'src/user/entities/user.entity';
 //cambio
 @ApiTags('Docs')
 @Controller('docs')
@@ -40,6 +43,22 @@ export class DocsController {
     @Param('enrollmentId') id: string,
   ) {
     const pdfBuffer = await this.pdfService.generatePdfWithQRCodesStudent(+id);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=carnets.pdf');
+    res.send(pdfBuffer);
+  }
+
+  @Get('download-contract')
+  @ApiOperation({
+    summary: 'download pdf with carnets students of Activity classroom',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Pdf with carnets of de classroom',
+  })
+  @Auth()
+  async downloadContract(@Res() res: Response, @GetUser() user: User) {
+    const pdfBuffer = await this.pdfService.generatePdfContract(user);
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', 'attachment; filename=carnets.pdf');
     res.send(pdfBuffer);
