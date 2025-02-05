2fbb635c5462520d91dc7f0544fd8be19d24d4ab
carlos jhardel <orellano428@gmail.com>
2025-02-04 23:40:27 -0500

    FEAT: url v3

--- src/enrollment/slack.service.ts
+++ src/enrollment/slack.service.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 @Injectable()
 export class SlackService {
   private webhookUrl =
-    'https://hooks.slack.com/services/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';
+    'https://hooks.slack.com/services/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';
 
   async sendMessage(message: string) {
     await axios.post(this.webhookUrl, { text: message });
