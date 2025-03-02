4da9a44da0e72fad55a3fa5ee0747ee115b7739a
carlos jhardel <orellano428@gmail.com>
2025-03-01 22:29:12 -0500

    DOC: documentation

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -43,6 +43,46 @@ export class StudentController {
   findAll() {
     return this.studentService.findAll();
   }
+  @ApiParam({
+    name: 'searchTerm',
+    required: false,
+    description: 'termino a buscar',
+    type: String,
+  })
+  @ApiParam({
+    name: 'yearId',
+    required: false,
+    description: 'id del año a filtrar',
+    type: String,
+  })
+  @ApiParam({
+    name: 'campusId',
+    required: false,
+    description: 'id de la sede a filtrar',
+    type: String,
+  })
+  @ApiParam({
+    name: 'levelId',
+    required: false,
+    description: 'id del nivel a filtrar',
+    type: String,
+  })
+  @ApiParam({
+    name: 'page',
+    required: false,
+    description: 'parámetro de paginación',
+    type: String,
+  })
+  @ApiParam({
+    name: 'limit',
+    required: false,
+    description: 'parámetro de paginación',
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'search matches',
+  })
   @Get('/search')
   findStudents(
     @Query() searchDto: SearchEstudiantesDto,
