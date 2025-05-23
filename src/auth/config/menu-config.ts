eb3c115557fccb8ce8049bc09798dfcf9b61769a
carlos jhardel <orellano428@gmail.com>
2025-05-23 16:50:38 -0500

    FIX: option areas

--- src/auth/config/menu-config.ts
+++ src/auth/config/menu-config.ts
@@ -421,6 +421,23 @@ export const AdminMenu = {
 
           subMenu: [],
         },
+        {
+          id: 10,
+          label: 'Cronograma',
+          icon: 'module',
+          link: '/admin/enrollment/schedule',
+          permissions: ['configuracion-cronograma'],
+
+          subMenu: [],
+        },
+        {
+          id: 11,
+          label: 'Area x Cursos',
+          icon: 'module',
+          link: '/admin/configuration/area',
+          permissions: ['configuracion-area'],
+          subMenu: [],
+        },
       ],
     },
   ],
