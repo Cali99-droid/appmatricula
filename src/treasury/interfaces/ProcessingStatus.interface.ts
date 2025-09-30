d1c662b8bf9a6a9dfcdc53013ca384b24bcb7a30
carlos jhardel <orellano428@gmail.com>
2025-09-30 14:02:30 -0500

    DETAIL: PAYMENTS

---
+++ src/treasury/interfaces/ProcessingStatus.interface.ts
@@ -0,0 +1,8 @@
+/**pending/processing/success/error */
+export enum ProcessingStatusInterface {
+  default = 'default',
+  pending = 'pending',
+  processing = 'processing',
+  success = 'success',
+  error = 'error',
+}
