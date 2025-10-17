81f65b5f13d02a412b6dab9a619a62d5d4942ba6
carlos jhardel <orellano428@gmail.com>
2025-10-17 17:06:27 -0500

    FEAT: send report

--- src/academic_structure/academic_records/academic_records.service.ts
+++ src/academic_structure/academic_records/academic_records.service.ts
@@ -775,7 +775,7 @@ export class AcademicRecordsService {
           where: {
             activityClassroom: {
               grade: {
-                id: In([10, 11, 12, 13, 14]),
+                id: In([1, 2, 3, 4, 5, 6, 7, 8, 9]),
               },
             },
             status: Status.MATRICULADO,
