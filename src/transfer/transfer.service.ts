105fc300a9f5c78b6ad986795e02be4f655d25e5
carlos jhardel <orellano428@gmail.com>
2025-12-22 10:34:33 -0500

    FIX: search auth

--- src/transfer/transfer.service.ts
+++ src/transfer/transfer.service.ts
@@ -255,7 +255,7 @@ export class TransfersService {
       await this.activityClassroomService.getIdsByLevelIdCampusIdAndCodes(
         campusId,
         levelId,
-        user.resource_access['client-test-appae'].roles,
+        user.resource_access['appcolegioae'].roles,
       );
     const resquestsOptions: any = {
       where: {
@@ -268,8 +268,8 @@ export class TransfersService {
     };
 
     if (
-      user.resource_access['client-test-appae'].roles.includes('secretaria') &&
-      !user.resource_access['client-test-appae'].roles.includes(
+      user.resource_access['appcolegioae'].roles.includes('secretaria') &&
+      !user.resource_access['appcolegioae'].roles.includes(
         'administrador-colegio',
       )
     ) {
@@ -490,7 +490,7 @@ export class TransfersService {
 
   async findMeetingsByUser(user: KeycloakTokenPayload): Promise<any[]> {
     const us = await this.userService.findByEmail(user.email);
-    const roles = user.resource_access['client-test-appae'].roles;
+    const roles = user.resource_access['appcolegioae'].roles;
     let status;
 
     const data = await this.transferMeetingRepository.find({
@@ -700,7 +700,7 @@ export class TransfersService {
     user: KeycloakTokenPayload,
   ): Promise<TransferReport[]> {
     if (
-      user.resource_access['client-test-appae'].roles.includes(
+      user.resource_access['appcolegioae'].roles.includes(
         'cordinador-academico',
       )
     ) {
